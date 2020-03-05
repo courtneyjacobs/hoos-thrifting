e20c1090a801dacb85a4d17e95f234d3dfd54235
Amara V <amarav@d-172-25-235-195.dhcp.virginia.edu>
2020-03-05 11:46:44 -0500

    Revised promocode JS, added buttons, updated Contact JS.

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+/**
+ * Author: Courtney Jacobs (cj5he), Amara Vo (av2jf)
+*/
+
 
 /*
-	sell.html
+ * sell.html
 */
 
 var items = [];
 
 // Valid Inputs
 
-// Creates <p> with items populated
+//*** Creates <p> with items populated
 function createItems() {
 	document.getElementById("createdItems").innerHTML ="<p>Your Items</p>";
 	var i=0;
@@ -20,7 +24,7 @@ function createItems() {
 	)
 }
 
-// Populate object from form
+//*** Populate object from form
 function populate() {
 	var newItem = {
 	    category:"",
@@ -47,9 +51,8 @@ function populate() {
 	document.getElementById("sell-form").reset();
 }
 
-// Check if price is within range.
+//*** Check if entered price is within range.
 function validPrice() {
-
 	var price = parseFloat(document.getElementById("price").value);
 	if (price <= 1000 && price >= 1) {
 		setEarnings();
@@ -59,57 +62,84 @@ function validPrice() {
 		var youEarnError = document.getElementById("youearnError");
 		youEarnError.innerHTML = "Value must be less than or equal to $1000.00";
 	}
-
 }
 
-// Update earnings field as user types in price.
+//*** Update earnings field as user types in price.
 function setEarnings() {
-
 	var price = parseFloat(document.getElementById("price").value);
 	var fee = 0.1*price;
 	var earnings = price - fee; 
 
 	var youEarn = document.getElementById("youearn");
 	youEarn.value = "$" + earnings;
-
 }
 
 /*
-	contact.html
+ * contact.html
 */
 
-// Show confirmation message
+//*** Show confirmation message after successful submission.
 function confirmMessage() {
-	// once Submit button is clicked
-	event.preventDefault();			// prevents page from refreshing
-	document.getElementById("contact-form").style.display="none";
-	document.getElementById("Confirm-Submission").style.display="block";
 
+	// contact form
+	if (document.getElementById("contact-form")) {
+		var name = document.getElementById('FName').value;
+		event.preventDefault();																// prevents page from refreshing
+		document.getElementById("Confirm-Text").innerHTML = "Thank you " + name + "! We will get in touch with you soon.";
+		document.getElementById("contact-form").style.display="none";
+		document.getElementById("Confirm-Submission").style.display="block";
+	}
+
+	// fundraise form
+	if (document.getElementById("fundraise-form")){	
+		
+		// if promo is invalid
+		if (document.getElementById("promoError").style.visibility == "visible") {
+			event.preventDefault();															// prevents form from submitting
+			document.getElementById("promo").focus();		
+		}
+		else {
+			var name = document.getElementById('FName').value;
+			event.preventDefault();															// prevents page from refreshing
+			document.getElementById("fundraise-form").style.display="none";
+			document.getElementById("Confirm-Submission").style.display="block";
+			document.getElementById("Confirm-Text").innerHTML = "Thank you " + name + ", good luck with your Fundraiser!";
+		}
+	}
 }
 
 /*
-	fundraise.html
+ * fundraise.html
 */
-var pastPromo = ["GOPUFF", "UVA20", "UVAUPC", "H"];				// global list - all code should be capitalized
+var pastPromo = ["GOPUFF", "UVA20", "UVAUPC", "H"];										// global list - all code should be capitalized
 
+//*** Verify if promo code can be used.
 function checkPromo() {
 	var enteredPromo = document.getElementById("promo").value.toUpperCase();
 	var status = document.getElementById("promoError").style.visibility;
 
 	// if error text is hidden
-	if (status == "hidden") {		
+	if (status == "hidden") {	
+
+		if (document.getElementById("submitButton").disabled) {
+			document.getElementById("submitButton").disabled="false";
+		}			// if disabled button
+
 		if (pastPromo.includes(enteredPromo) && (enteredPromo.length >= 3)) {			// if promo already exists
 			document.getElementById("promoError").style.visibility="visible";			// show error message
 		}
 		else{																			// combo of letters isn't in list yet
-			// add promo code to database list if submit button is clicked
+			//FUTURE: add promo code to database list if submit button is clicked
 		}
+		
 	}
 	// if error text is already shown
 	else {
-		document.getElementById("promoError").style.visibility="hidden";
-		checkPromo();		// call function again
+		document.getElementById("promoError").style.visibility="hidden";				// hide the text
+		checkPromo();																	// call function again
 	}
+
+
 }
 
 
