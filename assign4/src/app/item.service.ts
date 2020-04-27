e2dcb5bd4426bae3ca5318951238d2a9bb7ae2a6
Amara V <amarav@Amaras-MacBook-Pro.local>
2020-04-26 20:33:45 -0400

    added ngClass

--- assign4/src/app/item.service.ts
+++ assign4/src/app/item.service.ts
@@ -14,7 +14,7 @@ export class ItemService {
   constructor(private http: HttpClient) { }
 
   sendItemRequest(sort: any, dir: any): Observable<any> {
-    return this.http.post('http://localhost/hoos-thrifting/php/shop-db.php', {sort, dir}, {responsetype : 'json'});
+    return this.http.post('http://localhost/hoos-thrifting/php/shop-db.php', {sort, dir});
   }
 
   getItem(sort: string, dir: string): Observable<Item[]> {
